1347e511fc97426ae5bac05ca87ed589c2adfdf5
+ <+>
2022-08-22 11:35:43 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-// Write your solution here!
+
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+  console.log(cats)
+}
+destructivelyAppendCat()
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+    console.log(cats)
+}
+destructivelyPrependCat()
+
+function  destructivelyRemoveLastCat(){
+    cats.pop()
+    console.log(cats)
+}
+destructivelyRemoveLastCat();
+
+function  destructivelyRemoveFirstCat(){
+    cats.shift()
+    console.log(cats)
+}
+destructivelyRemoveFirstCat();
+
+function appendCat(name){
+    var newArray = cats.slice();
+    // or ES6 way
+    // var newArray = [...kittens];
+    newArray.push("Broom")
+    return newArray
+  }
+  appendCat();
+
+/*function  appendCat(){
+    
+    const catsCopy=[...cats]
+    catsCopy.push('Broom')
+    console.log(catsCopy)
+}
+*/
+
+function prependCat(name){
+    const catArray=cats.slice();
+    catArray.unshift('Arnold')
+    
+    return catArray
+}
+prependCat();
+
+function  removeLastCat(){
+    const lastCat=cats.slice();
+    lastCat.pop()
+    
+    return lastCat
+}
+removeLastCat();
+
+function  removeFirstCat(){
+    const firstCat=cats.slice(1)
+    
+    return firstCat
+}
+removeFirstCat();
+
+    
+//cats.unshift();
+//cats.pop();
+//cats.shift();
